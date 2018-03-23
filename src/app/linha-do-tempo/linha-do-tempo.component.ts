874401e8b7e4a02a2d2e5d5fd1a7d0b84953dd27
pereiraneto <pereira.neto.12@gmail.com>
2018-03-22 23:14:18 -0300

    Atividade 3 completa

--- src/app/linha-do-tempo/linha-do-tempo.component.ts
+++ src/app/linha-do-tempo/linha-do-tempo.component.ts
@@ -6,5 +6,13 @@ import { Post } from "./../post/post.model";
     templateUrl: './linha-do-tempo.component.html',
 })
 export class LinhaDoTempoComponent{
-    posts: Post[] = [];
+    posts: Post[] = [
+        new Post(1, "Pereira", "Muito bom o jogo hoje", 10),
+        new Post(2, "Jackson", "Faltou mais gols", 8),
+        new Post(3, "UrubuMIlGRAU", "Alô TITE !! Vinicius Jr também é brasileiro !", 100)
+    ];
+
+    capturaEvent(e){
+        console.log(e);
+    }
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Post } from 'app/post/post.model';
+import { log } from 'util';
 
 @Component({
     selector: 'app-post',
     templateUrl: './post.component.html',
 })
 export class PostComponent{
-    nome: string = "Pereira";
-    mensagem: string = "Primeiro Post";
+    @Input() post: Post;
+    @Output() recebeuLike: EventEmitter<Post> = new EventEmitter<Post>();
+    fotoLike: string = "../../assets/facebook-velho-botao-curtir.png";
+
+    incrementarLike():void{
+        this.post.qtdLikes++;
+        this.recebeuLike.emit(this.post);
+    }
 }
